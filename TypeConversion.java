a1845d3083209b8988c99e0b71bbc7e76c86703e
satyapal49 <satyapalraj49@gmail.com>
2026-06-05 10:31:57 +0530

    sdcs

--- TypeConversion.java
+++ TypeConversion.java
@@ -1,11 +1,20 @@
+import java.util.*;
+
 public class TypeConversion {
     public static void main (String[] args) {
         // int a = 10;
         // float b = a; // Implicit type conversion (int to float)
         // System.out.println("Value of b: " + b);
 
-        int c = 155;
-        byte d = (byte) c; // Explicit type conversion (int to byte)
-        System.out.println("Value of d: " + d);
+         // Read an integer from the user and store it in a float variable (implicit type conversion)
+        try(Scanner sc = new Scanner(System.in)) {  
+            System.out.print("Enter an integer: "); 
+            float a = sc.nextInt(); 
+            System.out.println("Value of a: " + a); 
+        }
+
+        // int c = 155;
+        // byte d = c; // Explicit type conversion (int to byte) - This will cause a compile-time error due to potential data loss
+        // System.out.println("Value of d: " + d);
     }
 }
---
+++ TypeCoversion.java
@@ -0,0 +1,5 @@
+public class TypeCoversion {
+    public static void main(String[] args) {
+        
+    }
+}
