ed7f92769ae3d3408286ae93b4e8ddd860398b52
satyapal49 <satyapalraj49@gmail.com>
2026-06-05 11:13:50 +0530

    Explicit type conversion (float to int)

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        float a = 10.52f;
+        int b = (int) a; // Explicit type conversion (float to int)
+        System.out.println("Value of b: " + b); // Output will be 10, as the decimal part is truncated during the cast
+    }
+}
--- TypeCoversion.java
+++
@@ -1,5 +0,0 @@
-public class TypeCoversion {
-    public static void main(String[] args) {
-        
-    }
-}
