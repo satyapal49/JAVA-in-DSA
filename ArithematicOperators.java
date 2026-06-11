5fe2a0414116c8d4a779bd1504bdb2f18a3480cc
satyapal49 <satyapalraj49@gmail.com>
2026-06-11 10:44:54 +0530

    Arithematic Operator

---
+++ ArithematicOperators.java
@@ -0,0 +1,11 @@
+public class ArithematicOperators {  // Class declaration
+    public static void main(String[] args) {  // Main method - entry point of the program
+        int a = 5;  // Declare and initialize variable a
+        int b = 10;  // Declare and initialize variable b
+        System.out.println("Addition: " + (a + b)); // Addition
+        System.out.println("Subtraction: " + (a - b)); // Subtraction
+        System.out.println("Multiplication: " + (a * b)); // Multiplication
+        System.out.println("Division: " + (a / b));  // Division
+        System.out.println("Modulus: " + (a % b));  // Modulus ( remainder of division)
+    }  // End of main method
+}  // End of class ArithematicOperators
