b939441d497c5b483fc2b80aa61bd7f421a41720
satyapal49 <satyapalraj49@gmail.com>
2026-06-11 14:40:31 +0530

    Relational operators compare two values and return a boolean result.

---
+++ RelationalOperators.java
@@ -0,0 +1,14 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        // Relational operators compare two values and return a boolean result.
+
+        int a = 5;  // Declare and initialize variable a
+        int b = 10; // Declare and initialize variable b
+        System.out.println("a == b: " + (a == b)); // Equal to
+        System.out.println("a != b: " + (a != b)); // Not equal to
+        System.out.println("a > b: " + (a > b));   // Greater than
+        System.out.println("a < b: " + (a < b));   // Less than
+        System.out.println("a >= b: " + (a >= b)); // Greater than or equal to
+        System.out.println("a <= b: " + (a <= b)); // Less than or equal to
+    }
+}
