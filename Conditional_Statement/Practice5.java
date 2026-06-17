ad815364d4330990cd94f23d98f42f45052694e3
satyapal49 <satyapalraj49@gmail.com>
2026-06-17 17:04:40 +0530

    leap year

--- Conditional_Statement/Practice5.java
+++ Conditional_Statement/Practice5.java
@@ -6,7 +6,7 @@ public class Practice5 {
         System.out.println("Check Whether a Year is a Leap Year or Not:");
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
-        String result =  (year %4 == 0) ? "Leap Year" : "Not Leap Year";
+        String result =  (year %4 == 0 && year % 100 != 0 || year % 400 == 0) ? "Leap Year" : "Not Leap Year";
         System.out.println("The year " + year + " is " + result);
     }
 }
